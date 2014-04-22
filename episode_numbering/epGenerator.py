b2564c189d8c71713d4cbc919f7a9bef2e5c63a7
John Okahata <--global>
2014-04-22 16:14:04 -0700

    EpGen: Repeat questions if range incorrect

--- episode_numbering/epGenerator.py
+++ episode_numbering/epGenerator.py
@@ -3,20 +3,30 @@ script, filename = argv
 
 print 'Opening the file %r...' % filename
 target = open(filename, 'w')
-
+is_valid_range = False
 season = raw_input("Season Number?: ")
-episodes = raw_input("Number of episodes?: ")
-start_num = raw_input("Episode to start counting from (Leave blank for default): ")
-if len(start_num) == 0:
-	start_num = 1
+
+while(not is_valid_range):
+	start_num = raw_input("Episode to start counting from (Leave blank for default): ")
+	episodes = raw_input("Total number of episodes?: ")
+	if len(start_num) == 0:
+		start_num = 1
+	if (start_num > episodes):
+		print 'Error: You starting passed the number of episodes'
+	else:
+		is_valid_range = True
+	if (start_num == episodes):
+		print 'Come on you could have done that yourself...'
 
 print 'Formatting season...'
 s = "S"
-if(season < 10):
+if (len(season) == 0):
+	s += "1"
+elif(season < 10):
 	s += "0"
 season = s + season + " "
 
-print 'Concatenating strings...'
+print 'Writing to file %r...' % filename
 for i in range(int(start_num), int(episodes) + 1):
 	e = "E"
 	if(i < 10):
