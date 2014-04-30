7a9705bc7e6a21ab722dcb1f1220f753789569f4
John Okahata <--global>
2014-04-29 22:02:48 -0700

    epGen: Add possibility to generate from command line arguments

--- episode_numbering/epGenerator.py
+++ episode_numbering/epGenerator.py
@@ -1,18 +1,49 @@
 from sys import argv
-if len(argv) == 1:
+#short is a variable to skip through to processing
+
+
+#python epGenerator.py <number or filename>
+if len(argv) == 2:
+	#Pass in number
+	try:
+		episodes = int(s)
+		short = True
+	#Pass in string for filename
+	except ValueError:
+		script, filename = argv
+#python epGenerator.py <number> <number or filename>
+elif len(argv) == 3: 
+	try: #Pass in two numbers
+		season = argv[1]
+		episodes = int(argv[2])
+		short = True
+	#Pass in string for filename
+	except ValueError:
+		episodes = int(argv[1])
+		filename = argv[2]
+
+#If no arguments given or shortened form
+if len(argv) == 1 or short:
 	filename = 'output_epGen'
 	print 'No filename specified'
 	print 'Generating output file %r...' % filename
-else:
-	script, filename = argv
+	
 print 'Opening the file %r...' % filename
 target = open(filename, 'w')
 is_valid_range = False
-season = raw_input("Season Number?: ")
 
-while(not is_valid_range):
+while(True):
+	if short:
+		start_num = 1
+		break;
+	if (is_valid_range):
+		break;
+	if (season is None):
+		season = raw_input("Season Number?: ")
+
 	start_num = raw_input("Episode to start counting from (Leave blank for default): ")
 	episodes = raw_input("Total number of episodes?: ")
+
 	if len(start_num) == 0:
 		start_num = 1
 	if (int(start_num) > int(episodes)):
@@ -21,6 +52,7 @@ while(not is_valid_range):
 		is_valid_range = True
 	if (start_num == episodes):
 		print 'Come on you could have done that yourself...'
+
 
 print 'Formatting season...'
 s = "S"
